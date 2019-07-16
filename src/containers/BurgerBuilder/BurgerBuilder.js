ee61760feac595e9bbcb001ba32e450aa836bc81
Peter Sims <ragtimesims@gmail.com>
2019-07-16 11:41:26 -0400

    fix import and set up posting to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
 	salad: 0.5,
 	cheese: 0.5,
@@ -72,7 +74,25 @@ class BurgerBuilder extends Component {
 		this.setState({ purchasing: false });
 	};
 	purchaseContinueHandler = () => {
-		alert('You continued');
+		// alert('You continued');
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: 'Peter',
+				address: {
+					street: 'Testing',
+					zipCode: '23423',
+					country: 'USA'
+				},
+				email: 'test@test.com'
+			},
+			deliveryMethod: 'fastest'
+		};
+		axios
+			.post('/orders.json', order)
+			.then((response) => console.log(response))
+			.catch((error) => console.log(error));
 	};
 	render() {
 		const disabledInfo = {
