f30368e410ea0566dd973423ae9bd41a262d30ed
Peter Sims <ragtimesims@gmail.com>
2019-07-18 10:36:57 -0400

    form routing and submit to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,34 +83,11 @@ class BurgerBuilder extends Component {
 		this.setState({ purchasing: false });
 	};
 	purchaseContinueHandler = () => {
-		// this.setState({ loading: true });
-		// // alert('You continued');
-		// const order = {
-		// 	ingredients: this.state.ingredients,
-		// 	price: this.state.totalPrice,
-		// 	customer: {
-		// 		name: 'Peter',
-		// 		address: {
-		// 			street: 'Testing',
-		// 			zipCode: '23423',
-		// 			country: 'USA'
-		// 		},
-		// 		email: 'test@test.com'
-		// 	},
-		// 	deliveryMethod: 'fastest'
-		// };
-		// axios
-		// 	.post('/orders.json', order)
-		// 	.then((response) => {
-		// 		this.setState({ loading: false, purchasing: false });
-		// 	})
-		// 	.catch((error) => {
-		// 		this.setState({ loading: false, purchasing: false });
-		// 	});
 		const queryParams = [];
 		for (let i in this.state.ingredients) {
 			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
 		}
+		queryParams.push('price=' + this.state.totalPrice);
 		const queryString = queryParams.join('&');
 		this.props.history.push({
 			pathname: '/checkout',
