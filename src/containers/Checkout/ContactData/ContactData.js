1d554db403c29e0c81ae1b0ae3728b5bbfedef3f
Peter Sims <ragtimesims@gmail.com>
2019-07-20 16:17:01 -0400

    two way binding on form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -90,6 +90,15 @@ class ContactData extends Component {
 			});
 	};
 
+	handleChange = (event, inputIdentifier) => {
+		const updatedOrderForm = {
+			...this.state.orderForm
+		};
+		const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
+		updatedFormElement.value = event.target.value;
+		updatedOrderForm[inputIdentifier] = updatedFormElement;
+		this.setState({ orderForm: updatedOrderForm });
+	};
 	render() {
 		const formElementsArray = [];
 		for (let key in this.state.orderForm) {
@@ -106,6 +115,7 @@ class ContactData extends Component {
 						elementType={formElement.config.elementType}
 						elementConfig={formElement.config.elementConfig}
 						value={formElement.config.value}
+						changed={(event) => this.handleChange(event, formElement.id)}
 					/>
 				))}
 				<Button btnType="Success" clicked={this.orderHandler}>
