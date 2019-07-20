cdb1c439c0a2ec1a9f3ad2d54a7ba69a7330b752
Peter Sims <ragtimesims@gmail.com>
2019-07-20 16:24:47 -0400

    form submission and firebase storage linked

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -65,19 +65,14 @@ class ContactData extends Component {
 	orderHandler = (e) => {
 		e.preventDefault();
 		this.setState({ loading: true });
+		const formData = {};
+		for (let formElementIdentifier in this.state.orderForm) {
+			formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+		}
 		const order = {
 			ingredients: this.props.ingredients,
 			price: this.props.price,
-			customer: {
-				name: 'Peter',
-				address: {
-					street: 'Testing',
-					zipCode: '23423',
-					country: 'USA'
-				},
-				email: 'test@test.com'
-			},
-			deliveryMethod: 'fastest'
+			orderData: formData
 		};
 		axios
 			.post('/orders.json', order)
@@ -108,7 +103,7 @@ class ContactData extends Component {
 			});
 		}
 		let form = (
-			<form>
+			<form onSubmit={this.orderHandler}>
 				{formElementsArray.map((formElement) => (
 					<Input
 						key={formElement.id}
@@ -118,9 +113,7 @@ class ContactData extends Component {
 						changed={(event) => this.handleChange(event, formElement.id)}
 					/>
 				))}
-				<Button btnType="Success" clicked={this.orderHandler}>
-					ORDER NOW
-				</Button>
+				<Button btnType="Success">ORDER NOW</Button>
 			</form>
 		);
 		if (this.state.loading) {
