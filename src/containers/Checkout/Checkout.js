c162c6434a1b58eb1995f448b70d3cbd1c83d4a6
Peter Sims <ragtimesims@gmail.com>
2019-08-04 16:47:02 -0400

    checkout refactor

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
 import { connect } from 'react-redux';
 
@@ -14,16 +14,20 @@ class Checkout extends Component {
 		this.props.history.replace('/checkout/contact-data');
 	};
 	render() {
-		return (
-			<div>
-				<CheckoutSummary
-					ingredients={this.props.ings}
-					checkoutCancelled={this.checkoutCancelledHandler}
-					checkoutContinued={this.checkoutContinuedHandler}
-				/>
-				<Route path={this.props.match.path + '/contact-data'} component={ContactData} />
-			</div>
-		);
+		let summary = <Redirect to="/" />;
+		if (this.props.ings) {
+			summary = (
+				<div>
+					<CheckoutSummary
+						ingredients={this.props.ings}
+						checkoutCancelled={this.checkoutCancelledHandler}
+						checkoutContinued={this.checkoutContinuedHandler}
+					/>
+					<Route path={this.props.match.path + '/contact-data'} component={ContactData} />
+				</div>
+			);
+		}
+		return <div>{summary}</div>;
 	}
 }
 
