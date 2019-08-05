96b56cd94c1da0759b66ed1233a44ab444884a6c
Peter Sims <ragtimesims@gmail.com>
2019-08-05 14:18:21 -0400

    create and route authorization form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,6 +8,7 @@ const navigationItems = () => (
 			BurgerBuilder
 		</NavigationItem>
 		<NavigationItem link="/orders">Orders </NavigationItem>
+		<NavigationItem link="/auth">Sign In </NavigationItem>
 	</ul>
 );
 
